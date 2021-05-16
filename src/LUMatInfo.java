ccf46722f33ba055476aae21f940a29c66b154b2
Joel Hwang <joel.c.hwang@gmail.com>
2021-05-16 09:04:23 +0900

    retrieve description

--- src/LUMatInfo.java
+++ src/LUMatInfo.java
@@ -197,140 +197,111 @@ public class LUMatInfo{
         String __EVENTVALIDATION = doc.select("input[name=__EVENTVALIDATION]").val();
 
         StringBuffer buffer = new StringBuffer(); 
-        buffer.append("ucFilter_ToolkitScriptManager1_HiddenField").append("=").append(ucFilter_ToolkitScriptManager1_HiddenField).append("&"); 
-        buffer.append("__EVENTTARGET").append("=").append(__EVENTTARGET).append("&"); 
-        buffer.append("__EVENTARGUMENT").append("=").append(__EVENTARGUMENT).append("&"); 
-        buffer.append("__LASTFOCUS").append("=").append(__LASTFOCUS).append("&"); 
-        buffer.append("__VIEWSTATE").append("=").append(__VIEWSTATE).append("&"); 
-        buffer.append("__VIEWSTATEGENERATOR").append("=").append(__VIEWSTATEGENERATOR).append("&"); 
-        buffer.append("__EVENTVALIDATION").append("=").append(__EVENTVALIDATION).append("&"); 
+        buffer.append("ucFilter_ToolkitScriptManager1_HiddenField").append("=").append(URLEncoder.encode(ucFilter_ToolkitScriptManager1_HiddenField, StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__EVENTTARGET").append("=").append(URLEncoder.encode(__EVENTTARGET,StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__EVENTARGUMENT").append("=").append(URLEncoder.encode(__EVENTARGUMENT,StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__LASTFOCUS").append("=").append(URLEncoder.encode(__LASTFOCUS,StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__VIEWSTATE").append("=").append(URLEncoder.encode(__VIEWSTATE,StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__VIEWSTATEGENERATOR").append("=").append(URLEncoder.encode(__VIEWSTATEGENERATOR,StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("__EVENTVALIDATION").append("=").append(URLEncoder.encode(__EVENTVALIDATION,StandardCharsets.UTF_8)).append("&"); 
         buffer.append("ucFilter$dd1Row1Field").append("=").append("1").append("&"); 
-        buffer.append("ucFilter$dd1Row1Operator").append("=").append("=").append("&"); 
-        buffer.append("ucFilter$cb1Row1$TextBox").append("=").append("LU2C/CL 3446").append("&"); 
+        buffer.append("ucFilter$dd1Row1Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&"); 
+        buffer.append("ucFilter$cb1Row1$TextBox").append("=").append(URLEncoder.encode("LU2C/CL 3446",StandardCharsets.UTF_8)).append("&"); 
         buffer.append("ucFilter$cb1Row1$HiddenField").append("=").append("-2").append("&"); 
 
         buffer.append("ucFilter$dd1Row2Field").append("=").append("2").append("&");
-        buffer.append("ucFilter$dd1Row2Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd1Row2Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb1Row2$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb1Row2$HiddenField").append("=").append("0").append("&");
-        buffer.append("ucFilter$dd1Row3Field").append("=").append("3");
-        buffer.append("ucFilter$dd1Row3Operator").append("=").append("=");
-        buffer.append("ucFilter$cb1Row3$TextBox").append("=").append("");
-        buffer.append("ucFilter$cb1Row3$HiddenField").append("=").append("0");
+        buffer.append("ucFilter$dd1Row3Field").append("=").append("3").append("&");
+        buffer.append("ucFilter$dd1Row3Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
+        buffer.append("ucFilter$cb1Row3$TextBox").append("=").append("").append("&");
+        buffer.append("ucFilter$cb1Row3$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd1Row4Field").append("=").append("4").append("&");
-        buffer.append("ucFilter$dd1Row4Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd1Row4Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb1Row4$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb1Row4$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd1Row5Field").append("=").append("5").append("&");
-        buffer.append("ucFilter$dd1Row5Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd1Row5Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb1Row5$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb1Row5$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd1Row6Field").append("=").append("6").append("&");
-        buffer.append("ucFilter$dd1Row6Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd1Row6Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb1Row6$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb1Row6$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row1Field").append("=").append("1").append("&");
-        buffer.append("ucFilter$dd2Row1Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row1Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row1$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row1$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row2Field").append("=").append("2").append("&");
-        buffer.append("ucFilter$dd2Row2Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row2Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row2$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row2$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row3Field").append("=").append("3").append("&");
-        buffer.append("ucFilter$dd2Row3Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row3Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row3$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row3$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row4Field").append("=").append("4").append("&");
-        buffer.append("ucFilter$dd2Row4Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row4Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row4$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row4$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row5Field").append("=").append("5").append("&");
-        buffer.append("ucFilter$dd2Row5Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row5Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row5$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row5$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd2Row6Field").append("=").append("6").append("&");
-        buffer.append("ucFilter$dd2Row6Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd2Row6Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb2Row6$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb2Row6$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$lbtnAdvancedSearch").append("=").append("Search").append("&");
         buffer.append("ucFilter$dd3Row1Field").append("=").append("1").append("&");
-        buffer.append("ucFilter$dd3Row1Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row1Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row1$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row1$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd3Row2Field").append("=").append("2").append("&");
-        buffer.append("ucFilter$dd3Row2Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row2Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row2$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row2$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd3Row3Field").append("=").append("3").append("&");
-        buffer.append("ucFilter$dd3Row3Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row3Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row3$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row3$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd3Row4Field").append("=").append("4").append("&");
-        buffer.append("ucFilter$dd3Row4Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row4Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row4$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row4$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd3Row5Field").append("=").append("5").append("&");
-        buffer.append("ucFilter$dd3Row5Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row5Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row5$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row5$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd3Row6Field").append("=").append("6").append("&");
-        buffer.append("ucFilter$dd3Row6Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd3Row6Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb3Row6$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb3Row6$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row1Field").append("=").append("1").append("&");
-        buffer.append("ucFilter$dd4Row1Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row1Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row1$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row1$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row2Field").append("=").append("2").append("&");
-        buffer.append("ucFilter$dd4Row2Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row2Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row2$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row2$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row3Field").append("=").append("3").append("&");
-        buffer.append("ucFilter$dd4Row3Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row3Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row3$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row3$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row4Field").append("=").append("4").append("&");
-        buffer.append("ucFilter$dd4Row4Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row4Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row4$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row4$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row5Field").append("=").append("5").append("&");
-        buffer.append("ucFilter$dd4Row5Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row5Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row5$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row5$HiddenField").append("=").append("0").append("&");
         buffer.append("ucFilter$dd4Row6Field").append("=").append("6").append("&");
-        buffer.append("ucFilter$dd4Row6Operator").append("=").append("=").append("&");
+        buffer.append("ucFilter$dd4Row6Operator").append("=").append(URLEncoder.encode("=",StandardCharsets.UTF_8)).append("&");
         buffer.append("ucFilter$cb4Row6$TextBox").append("=").append("").append("&");
         buffer.append("ucFilter$cb4Row6$HiddenField").append("=").append("0");
-        
-
-        /*BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        StringBuffer stringBuffer = new StringBuffer();
-        String inputLine;
-
-        while ((inputLine = bufferedReader.readLine()) != null)  {
-            stringBuffer.append(inputLine);
-        }
-        bufferedReader.close();
-
-        String response = stringBuffer.toString();*/
-        
-        /*
-        try (InputStream in = conn.getInputStream();
-             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-
-            byte[] buf = new byte[1024 * 8];
-            int length = 0;
-            while ((length = in.read(buf)) != -1) {
-                out.write(buf, 0, length);
-            }
-            
-            String output = new String(out.toByteArray(),"UTF-8");
-            System.out.println(output);
-            Document doc = Jsoup.parse(output);
-        }
-*/
-
         
         // 접속 해제
         conn.disconnect();
@@ -344,7 +315,7 @@ public class LUMatInfo{
         URL url = new URL("https://www.nikeconnect.com/Asia.CHN.Guangzhou.LTM_SSL/FWMCS_List_LTM.aspx");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        String param = URLEncoder.encode(buffer.toString(),StandardCharsets.UTF_8);
+        String param = buffer.toString();
         byte[] postData = param.getBytes( StandardCharsets.UTF_8 );
 
         conn.setRequestMethod("POST");
@@ -355,20 +326,20 @@ public class LUMatInfo{
         conn.setConnectTimeout(3000);
         conn.setReadTimeout(3000);
         
-        conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-        conn.setRequestProperty("Host","www.nikeconnect.com");
-        conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0");
-        conn.setRequestProperty("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        conn.setRequestProperty("Accept-Language","ko-KR,en-US;q=0.7,en;q=0.3");
+        conn.setRequestProperty("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+        conn.setRequestProperty("Accept-Language","ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7");
         conn.setRequestProperty("Accept-Encoding","identity");
-        conn.setRequestProperty("Origin","https://www.nikeconnect.com");
         conn.setRequestProperty("Connection","keep-alive");
-        conn.setRequestProperty("Referer","https://www.nikeconnect.com/irj/portal");
+        conn.setRequestProperty("Content-Length", Integer.toString(postData.length ));
+        conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
+        conn.setRequestProperty("Cookie", cookie);
+        conn.setRequestProperty("Host","www.nikeconnect.com");
+        conn.setRequestProperty("Origin","https://www.nikeconnect.com");
+        conn.setRequestProperty("Referer","https://www.nikeconnect.com/Asia.CHN.Guangzhou.LTM_SSL/FWMCS_List_LTM.aspx");
+        conn.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36");
         conn.setRequestProperty("Upgrade-Insecure-Requests","1");
         conn.setRequestProperty("Pragma","no-cache");
         conn.setRequestProperty("Cache-Control","no-cache");
-        conn.setRequestProperty("Cookie", cookie);
-        conn.setRequestProperty("Content-Length", Integer.toString(postData.length ));
         
         // 서버로 전송 
         try(DataOutputStream wr = new DataOutputStream(conn.getOutputStream())) {
@@ -394,8 +365,9 @@ public class LUMatInfo{
             }
             String output = new String(out.toByteArray(), "UTF-8");
             Document doc = Jsoup.parse(output);
-            String gvList = doc.select("tagble#gvList").text();
-            System.out.println(gvList);
+            String gvList = doc.select("table#gvList").text();
+            String desc = doc.select("table#gvList>tbody>tr:nth-child(2)>td:nth-child(9)").text();
+            System.out.println(desc);
         }
 
         // 접속 해제
